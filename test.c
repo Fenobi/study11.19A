58421b720999bae476909592e480550af8f47f45
Fenobi <13667739122@qq.com>
2022-11-19 22:36:32 +0800

    上课

---
+++ test.c
@@ -0,0 +1,179 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int i = 10;
+//
+//int main()
+//{
+//	//int i = 1;
+//	printf("%d", i);
+//	
+//	/*printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	printf("%d\n", sizeof(long double));*/
+//
+//	return 0;
+//}
+
+//#define MAX  4
+//
+//int i = 0;
+//
+//int sum(int x, int y)
+//{
+//	int i = 0;
+//	return i + x + y;
+//}
+//
+//int main()
+//{
+//	const int w = 0;
+//	printf("%d\n", MAX);
+//	int i = 0;
+//	int a = 0;
+//	a = sum(1, 2);
+//	printf("%d\n", i);
+//	scanf("%d", &i);
+//	printf("%d\n", i);
+//	printf("%d", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char a[10] = "abcdsdef";
+//	int len = strlen(a);
+//	printf("%d\n", len);
+//	printf("%s\n", a);
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	printf("\"\'");
+//	//printf("c:\code\test.c\n");
+//	printf("\a");
+//	return 0;
+//}
+
+//选择语句if
+//int main()
+//{
+//	int i = 5;
+//	int a = 1;
+//	if (a==i)
+//	{
+//		a = 2;
+//	}
+//	else if(a==2)
+//	{
+//		a = 3;
+//	}
+//	else
+//	{
+//		a = 4;
+//	}
+//	printf("%d\n", a);
+//	return 0;
+//}
+//switch
+//int main()
+//{
+//	int a = 1;
+//	switch (a)
+//	{
+//	case 1:
+//	{
+//		printf("%d", a);
+//		break;
+//	}
+//	case 2:
+//	{
+//		printf("%d", a);
+//		break;
+//	}
+//	default:
+//	{
+//		printf("%d", a);
+//		break;
+//	}
+//	}
+//
+//	return 0;
+//}
+
+
+
+//循环语句while
+//int main()
+//{
+//	int a = 0;
+//	while (a != 10000000)
+//	{
+//		printf("写代码：%d行\n", a);
+//		a = a + 1;
+//	}
+//	printf("我是代码王\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int a = 1;
+//	//a = i++;
+//	//printf("%d\n", a);//0
+//	//i=1
+//	//a = ++i;
+//	//printf("%d\n", a);//2
+//	for (i = 1; i < 100; i++)
+//	{
+//		a++;
+//		printf("%d ", a);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	写一个程序：1+2+3+4+....+10
+//	while
+//	for
+//	int sum = 0;
+//	int i = 1;
+//	while (i<=10)
+//	{
+//		sum = i + sum;//sum=10+9+8+7..1
+//		i++;
+//		printf("%d ", sum);
+//	}
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum = sum + i;
+//		sum += i;
+//		printf("%d ", sum);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	do 
+//	{
+//		printf("%d\n", i);
+//	} while (i);
+//	while (i)
+//	{
+//		printf("while");
+//	}
+//	return 0;
+//}
+
+
